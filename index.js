b6238c2078ac3ca2049188b58a0ccfc6c5e02cef
Xavier Cambar <xcambar@gmail.com>
2014-10-16 19:36:09 +0200

    Replaces DS.FixtureAdapter

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-/*global DS:true*/
+/*global DS:true, Ember:true*/
 'use strict';
 
 var Faker = require('faker');
+var fmt = Ember.String.fmt;
 
-var FakerAdapter = DS.FixtureAdapter.extend({});
+var FakerAdapter = DS.FixtureAdapter.extend({
+  fixturesForType: function(type) {
+    var typeKey = type.typeKey;
+    var fixturesForType = FakerAdapter.types[typeKey];
+    if (fixturesForType) {
+      var fixtures = Ember.A(fixturesForType);
+      return fixtures.map(function(fixture){
+        var fixtureIdType = typeof fixture.id;
+        if(fixtureIdType !== "number" && fixtureIdType !== "string"){
+          throw new Error(fmt('the id property must be defined as a number or string for fixture %@', [fixture]));
+        }
+        fixture.id = fixture.id + '';
+        return fixture;
+      });
+    }
+    return null;
+  },
+});
+
+FakerAdapter.types = {};
+
 module.exports = FakerAdapter;
